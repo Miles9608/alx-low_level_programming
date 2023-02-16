672f2c53dc5706c0ba12ea1c392991e4c6f6bca3
Simbarashe <visualnerds96@gmail.com>
2023-02-16 05:30:22 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <unistd.h>
-/** 
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
- * Return: Always 1
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
  */
-
 int main(void)
-
 {
-	write (2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
 	return (1);
 }
