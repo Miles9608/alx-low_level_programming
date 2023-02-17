25cce11d717d5845627facb188f576fe1a266d85
Simbarashe <visualnerds96@gmail.com>
2023-02-17 04:42:21 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: print all ther numbers of bass 16
+ * in lowercase followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char miles;
+
+	for (miles = '0'; milees <= '9' miles++)
+	{
+		putchar(miles);
+	}
+	for (miles = 'a'; miles <= 'f'; miles++)
+	{
+		putchar(miles);
+	}
+	putchar('\n');
+	return (0);
+}
