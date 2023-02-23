2a3b7d3f495148544c7067984fc7160e6bb980f4
Simbarashe <visualnerds96@gmail.com>
2023-02-23 01:44:54 -0800

    0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - main block
+ * Descption: Tests function that prints if interger
+ * is positive or nagative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	int = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
