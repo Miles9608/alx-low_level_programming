89bfc1b8036b90d8f51edf115715fc3bbc2f281e
Simbarashe Lungu <visualnerd96@gmail.com>
2023-03-30 08:36:43 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,27 @@
-[200~extern printf
+;--------------------------------------------------------------------------------------
+;
+; A 64-bit program in assembly that prints Hello, Holberton, followed by a new line.
+; Compiled using nasm and gcc.
+; nasm -f elf64 101-hello_holberton.asm && gcc 101-hello_holberton.o -o hello
+;
+;--------------------------------------------------------------------------------------
 
-	section .data
-msg:		db 	"Hello, Holberton", 0
-fmt:		db	"%s", 10, 0
+section .data
+format db "%s", 10, 0
+message db "Hello, Holberton", 0
 
-	section	.text
+section .text
+extern printf ;C function to be called
+global main ;main function
 
-	global	main
 main:
-	push	rbp
+	push rbp
+	mov rdi, format
+	mov rsi, message
+	mov rax, 0 ;can be xor rax, rax
+	call printf ;call C function.
 
-	mov	rdi,fmt
-	mov	rsi,msg
-	mov	rax,0
-	call	printf
+	pop rbp ;restor stack
 
-	pop	rbp
-
-	mov	rax,0
-	ret
+	mov rax, 0 ;normal, no error, return value
+	ret ;return
