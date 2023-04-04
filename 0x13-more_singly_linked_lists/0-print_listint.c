a6340dbbd2fc306310653902ec028eb9e4be5244
Simbarashe Lungu <visualnerd96@gmail.com>
2023-04-04 04:35:39 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @miles: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *miles)
+{
+    size_t num = 0;
+
+    while (miles)
+    {
+        printf("%d\n", miles->n);
+        num++;
+        miles = miles->next;
+    }
+
+    return (num);
+}
