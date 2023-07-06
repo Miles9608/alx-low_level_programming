97cdbfb4049861589e96ebde3452f3830ec7cfa9
Simbarashe Lungu <visualnerds96@gmail.com>
2023-07-06 04:20:29 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+/**
+ * get_endianness - Chicking endianess
+ *
+ * Return: void
+ */
+
+int get_endianness(void)
+{
+	int index;
+
+	index = 2;
+
+	if (index & 1)
+	{
+		return (0);
+	}
+
+	return (1);
+}
