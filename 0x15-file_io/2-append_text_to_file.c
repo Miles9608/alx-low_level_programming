78f2bbebda5c5f4f6fbd21e5e0b9cec6dbeab7e3
Simbarashe Lungu <visualnerds96@gmail.com>
2023-07-12 23:54:49 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,39 +1,44 @@
 #include "main.h"
 
 /**
- * create_file - Creating a new file
+ * append_text_to_file - Appending of text happens at the end of the line.
  *
- * @filename: Parameter description
+ * @filename: Pointer for const char
  *
- * @text_content: Parameter description
+ * @text_content: Pointer for char.
  *
  * Return: int
  */
-int create_file(const char *filename, char *text_content)
+
+
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int filePrint, length = 0, filePrintWrite;
+	int fileDescripOpen, fileDescripWrite, length = 0;
 
-	filePrint = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-
-	if (filePrint == -1)
+	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
+	fileDescripOpen = open(filename, O_WRONLY | O_APPEND);
+
+	if (fileDescripOpen == -1)
 	{
-		text_content = "";
+		return (-1);
 	}
+
+	if (text_content == NULL)
+		text_content = "";
 
 	while (text_content[length] != '\0')
 	{
 		length++;
 	}
 
-	filePrintWrite = write(filePrint, text_content, length);
+	fileDescripWrite = write(fileDescripOpen, text_content, length);
 
-	if (filePrintWrite == -1)
-	{
+	if (fileDescripWrite == -1)
 		return (-1);
-	}
-	close(filePrint);
+
+	close(fileDescripOpen);
+
 	return (1);
 }
